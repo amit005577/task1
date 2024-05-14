aed86c03fc9d6c2fb1cfe954f3233c959c6ad87d
amit <amitpatel005577@gmail.com>
2024-05-14 15:27:20 +0530

    hhh

--- App.tsx
+++ App.tsx
@@ -87,7 +87,7 @@ function App(): JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:dsddfsfd
+            Read the docs to discover what to do next:dsddfsfd ghjgjhgjhg
           </Section>
           <LearnMoreLinks />
         </View>
